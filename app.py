1faf378cef6a63a7d39706f752d46a00da392057
xiaobaicn-21 <josue2025cn@outlook.com>
2025-06-07 13:12:23 +0800

    finish main page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from team1.routes import team1_bp
 from team2.routes import team2_bp
 from team3.routes import team3_bp
 
 app = Flask(__name__)
+
+# 首页
+@app.route('/')
+def index():
+    return render_template('main_page.html')
 
 app.register_blueprint(team1_bp, url_prefix='/team1')
 app.register_blueprint(team2_bp, url_prefix='/team2')
